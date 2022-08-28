7ef8c99e86e19c438ae96515be1fb1b7de874061
xvanc <xvancm@gmail.com>
2022-08-28 18:40:49 -0500

    pcie refactor

--- spark/src/dev/mod.rs
+++ spark/src/dev/mod.rs
@@ -30,3 +30,49 @@
 
 pub mod fw_cfg;
 pub mod pcie;
+
+use core::mem::size_of;
+use libsa::extern_sym;
+
+pub struct DeviceDriver {
+    pub name: &'static str,
+    pub fdt_compat: Option<&'static [&'static str]>,
+    pub fdt_init: Option<fn(&fdt::Fdt, &fdt::node::FdtNode)>,
+    pub pci_compat: Option<&'static [&'static pcie::DriverCompat]>,
+    pub pci_init: Option<fn(&pcie::Device)>,
+}
+
+pub fn device_drivers() -> &'static [DeviceDriver] {
+    let drivers_start = extern_sym!(__device_drivers as DeviceDriver);
+    let drivers_end = extern_sym!(__end_device_drivers as DeviceDriver);
+    let len = (drivers_end.addr() - drivers_start.addr()) / size_of::<DeviceDriver>();
+
+    unsafe { core::slice::from_raw_parts(drivers_start, len) }
+}
+
+fn match_fdt_driver(node: &fdt::node::FdtNode) -> Option<&'static DeviceDriver> {
+    let node_compat = node.compatible()?;
+
+    device_drivers()
+        .iter()
+        .find(|driver| match driver.fdt_compat {
+            Some(driver_compat) => node_compat
+                .all()
+                .any(|compat| driver_compat.contains(&compat)),
+            None => false,
+        })
+}
+
+pub fn init(fdt: &fdt::Fdt) {
+    log::debug!("scanning device tree");
+    for node in fdt.all_nodes() {
+        if let Some(driver) = match_fdt_driver(&node) {
+            log::debug!("found driver: {} for node `{}`", driver.name, node.name);
+            if let Some(init_fn) = driver.fdt_init {
+                init_fn(fdt, &node);
+            } else {
+                log::debug!("driver missing fdt init fn");
+            }
+        }
+    }
+}
